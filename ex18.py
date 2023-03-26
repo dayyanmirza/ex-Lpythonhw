5dc091c195d8d682524dfab54ef85e61c476c0da
dayyanmirza <dayyanmirza15@gmail.com>
2023-03-26 18:24:24 +0100

    ex18.py

--- ex18.py
+++ ex18.py
@@ -6,7 +6,7 @@ def print_two(*args):
 # ok , that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
-
+    
 # this just takes one argument
 def print_one(arg1):
     print(f"arg1: {arg1}")
