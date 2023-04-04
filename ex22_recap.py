44f902b0483f5309042f780557f4828878082a97
dayyanmirza <dayyanmirza15@gmail.com>
2023-04-04 17:31:42 +0100

    Ex22 recap

---
+++ ex22_recap.py
@@ -0,0 +1,28 @@
+#print('I am 6\'2" tall.')
+
+#print('\a')
+
+#print('\b')
+
+#print('lol\f')
+
+#print('\n')
+
+#a = '\u004a\u0061'
+#print(a)    
+
+#txt = "LOL\rLOL"
+#print(txt)
+
+#print("LOL\v")
+
+#print("\tLOL")
+
+#A backslash followed by three integers will result in a octal value:
+#txt = "\110\145\154\154\157"
+#print(txt) 
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+#txt = "\x48\x65\x6c\x6c\x6f"
+#print(txt) 
+
