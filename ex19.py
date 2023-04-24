a2d3d5a3cc602ae082d49b72065a4eb5148bee42
dayyanmirza <dayyanmirza15@gmail.com>
2023-04-24 13:06:10 +0100

    ex 24

--- ex19.py
+++ ex19.py
@@ -3,7 +3,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
-
+# the variables in your functions are not connected to the variables in your script. Check down below:
 
 print("We can just give the function numbers directly:")
 cheese_and_crackers(20, 30)
