869b434dbc1a65e0bc3b46406b0e8e23aacda5c1
dayyanmirza <dayyanmirza15@gmail.com>
2023-05-19 17:47:05 +0100

    Added ex31

---
+++ ex31.py
@@ -0,0 +1,54 @@
+print("""You enter a dark room with two doors. Do you go through door #1, door #2 or door #3?""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake.")
+    print("What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your leg off. Good job!")    
+    else:
+        print(f"Well, doing {bear} is probably better.")
+        print("Bear runs away.")
+
+elif door == "2":
+    print("You stare into the endless abyss at Cthulhu's retina.")    
+    print("1. Blueberries")
+    print("2. Yellow jacket clothespins")
+    print("3. Understanding revolvers yelling melodies.")
+
+    insanity = input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello.")
+        print("Good job!")
+    else:
+        print("The insanity rots your eyes into a pool of muck.")
+        print("Good job!")
+
+if door == "3":
+    print("You see 100 armed soilders.")
+    print("What do you do?")
+    print("1. Scream at the top of your lungs.")
+    print("2. Stay silent.")
+
+    choice = input("> ")
+    
+    # if you change elif to if it will run the if statement and the else statment aswell. Try it if you want. 
+    if choice == "1":
+        print("You get demolished by the soilders.") 
+    elif choice == "2":
+        print("They leave you alone.")
+    else:
+        print("They steal everything you have, but you stay alive.")           
+
+else:
+    print("You stumble around and fall on a knife and die. Good job!")
+            
