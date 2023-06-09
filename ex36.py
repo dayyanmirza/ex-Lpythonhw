559978036208b323ff35aadb9b25897055df2305
dayyanmirza <dayyanmirza15@gmail.com>
2023-06-09 17:15:49 +0100

    Done the mirage game ex36

--- ex36.py
+++ ex36.py
@@ -6,27 +6,89 @@ print("This game is inspired by learn python3 the hardway:", script)
 print(f"Welcome to {game} - adventure game based in the desert.\n")
 
 # create the ending function
-
+def ending():
+    print("You go back to their tent and you see a pile of gold there.")
+    print("You could either (please input a number): 1) take the 5kg of gold, 2) leave it. ")
+ 
+    choice = int(input("> ")) 
+    
+    if choice == 1:
+        die("You don't learn your lesson do you. You get killed by the chief who always checks his stash.")
+    elif choice == 2:
+        print("You end up staying there ") 
+        print("You find a nice girl you like. The locals like you too and then you just stay for 20 years get married to the girl have kids.") 
+        print("After staying for 20 years they decide to return you back home.\n")
+        print("But then you confess that you lied this whole time. The chief gets angry and asks his guards to imprison you.\n")
+        print("You stay in the cell for a while and die of old age...\n")
+        print("For some reason though you wake up and feel as if nothing has changed cause guess what...you're still in the desert. It was all a dream!")
+        start()
+    else:
+        print("Sorry, that's not an option.")   
 
 # create the village function
+def village():
+    print("You are able to keep yourself hydrated and healthy with the remaining supplies.")
+    print("You stumble upon a village but your boat is stuck a bit too far outshore on a rock.")
+    print("You have two options (please input a number): 1) swim to the beach, 2) use your airhorn.")
 
+    choice = int(input("> "))
 
-# create the boat function
+    if choice == 1:
+        die("You don't learn your lesson do you. You drown.")
+    elif choice == 2:
+        print("People come to see what happened") 
+        print("You pretend to act like a local.")
+        print("They investigate and take you in.") 
+        ending() 
+    else:
+        print("Sorry, that's not an option.")    
+ 
+
+# create boat function
+def boat():
+    print("You reach the boat and it has supplies but it's too heavy to keep them all.")
+    print("You have to get rid of one of the three options (please input a number): 1)airhorn, 2)water bottle, 3)lighter.")
+
+    choice = int(input("> "))
+
+    if choice == 1:
+        die("You reach a village but your boat get stuck and you have no way of contacting locals so are left stranded and die.")
+    elif choice == 2:
+        die("Without the water bottle you die of dehydration.")
+    elif choice == 3:
+        print("Good choice.")
+        village()     
+    else:
+        print("Sorry, that's not an option.")    
 
 
 # create the river function
-#def river:
+def river():
+    print("You see a boat in the middle of a river.")
+    print("You have one of three options (please input a number): 1)swim to boat, 2)use the camel to swim, 3)walk around the river.")
+
+    choice = int(input("> "))
+
+    if choice == 1:
+        die("Unfortunately you drown.")
+    elif choice == 2:
+        print("Good choice.")
+        boat()
+    elif choice == 3:
+        die("You starve and die.")     
+    else:
+        print("Sorry, that's not an option.")     
 
 # create the backdoor function for the red_door room.
 def backdoor():
     print("You find a camel outside.")
-    print("You have three options: 1)stroke, 2)ride, 3)taunt.")
+    print("You have three options (please input a number): 1)stroke, 2)ride, 3)taunt.")
 
     choice = int(input("> "))
 
     if choice == 1:
         print("The camel becomes friendly with you and you ride it.")
-        #river()
+        river()
     elif choice == 2:
         die("The camel starts moving vigorously to make you fall of and you land on your head.")
     elif choice == 3:
@@ -55,7 +117,7 @@ def red_door():
             die("You die of dehydration.")
         elif choice == 'cut it' and door_open:
             print("You start drinking water from the cactus and feel better.")
-            print("While drinking you walk outside outside through a backdoor.")  
+            print("While drinking you walk outside through a backdoor.")  
             backdoor()     
         else:
             print("Sorry, that's not an option.")  
