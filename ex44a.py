97b03eb27ce6fed227c9a1dc402cc9b18bd16e5f
dayyanmirza <dayyanmirza15@gmail.com>
2023-08-12 17:46:39 +0100

    ex 44a

---
+++ ex44a.py
@@ -0,0 +1,16 @@
+# Implicit Inheritance
+class Parent(object):
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+
+class Child(Parent):
+    pass
+
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
