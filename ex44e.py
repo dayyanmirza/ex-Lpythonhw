4c5a7f6a6cbad0b1104ef59099035ba145f76d66
dayyanmirza <dayyanmirza15@gmail.com>
2023-08-17 14:54:35 +0100

    ex44e

--- ex44e.py
+++ ex44e.py
@@ -1,37 +1,36 @@
-# # Composition:
+# Composition:
+class Other(object):
 
-# class Other(object):
+    def override(self):
+        print("OTHER override()")
 
-#     def override(self):
-#         print("OTHER override()")
+    def implicit(self):
+        print("OTHER implicit()")
 
-#     def implicit(self):
-#         print("OTHER implicit()")
+    def altered(self):
+        print("OTHER altered()")
 
-#     def altered(self):
-#         print("OTHER altered()")
+class Child(object):
 
-# class Child(object):
+    def __init__(self):
+        self.other = Other() # creates an instance of the Other() class --> assigned to the self.other attribute
 
-#     def __init__(self):
-#         self.other = Other() # creates an instance of the Other() class --> assigned to the self.other attribute
+    def implicit(self):
+        self.other.implicit() # used to access the Other class implicit function. 
 
-#     def implicit(self):
-#         self.other.implicit() # used to access the Other class implicit function. 
+    def override(self):
+        print("CHILD override()")
 
-#     def override(self):
-#         print("CHILD override()")
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered() # used to access the Other class altered function
+        print("CHILD, AFTER OTHER altered()")
 
-#     def altered(self):
-#         print("CHILD, BEFORE OTHER altered()")
-#         self.other.altered() # used to access the Other class altered function
-#         print("CHILD, AFTER OTHER altered()")
+son = Child()
 
-# son = Child()
-
-# son.implicit()
-# son.override()
-# son.altered()
+son.implicit()
+son.override()
+son.altered()
 
 """
 Study Drills:
